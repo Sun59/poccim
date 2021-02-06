108e0af6e98f289d16ecb104d0cbd4500b044fa3
K2 Sunshine <hammouti.samir@gmail.com>
2021-02-06 21:40:45 +0100

    t

--- mkdocs-build-action/entrypoint.sh
+++ mkdocs-build-action/entrypoint.sh
@@ -15,7 +15,7 @@ chmod -R a+w /github/workspace
 # Build the Mkdocs site
 ####################################################
 
-sudo pip install mkdocs-minify-plugin>=0.2 -H foo
+pip install mkdocs-minify-plugin==0.3.0
 
 mkdocs build
 
